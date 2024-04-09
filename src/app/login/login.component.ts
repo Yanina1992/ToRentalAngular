869f1ecba46c53c2126ae5fdd6e78ae1e1600f2c
lurgoyf82 <lurgoyf82@hotmail.com>
2024-04-09 11:16:34 +0200

    Update app component HTML
    The existing HTML code of the app component has been cleaned up. This includes removal of the sample placeholders, extra comments and styles, thus making it a lot more readable and maintainable. This is a step towards making the application interface simple and user-friendly.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,14 +6,14 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  username: string = ''; // Initialize as empty string
+  email: string = ''; // Initialize as empty string
   password: string = ''; // Initialize as empty string
 
   constructor(private authService: AuthService) {}
 
   login(): void {
     // Since username and password are initialized, they're guaranteed to be strings here
-    this.authService.login(this.username, this.password).subscribe(() => {
+    this.authService.login(this.email, this.password).subscribe(() => {
       console.log('Login successful');
     }, error => {
       console.error('Login error', error);
